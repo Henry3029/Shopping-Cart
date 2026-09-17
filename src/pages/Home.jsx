99cf4fd04fd6da035ccf49cfd237459ee6fb0ec9
Henry goodluck chigozie <chigoziehenry852@gmail.com>
2026-09-17 12:03:37 +0000

    modigy Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css';
 
-import bigviewImage from '../assets/bigview-image.jpg';
 import birthdayImage from '../assets/birthday-image.jpg';
 import movieImage from '../assets/movie-image.jpg';
 
